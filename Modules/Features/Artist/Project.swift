20b0dde1a3b2dbfb458a291de178fc29f181be27
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-02 15:40:58 +0200

    Add supporting modules to Artist module

--- Modules/Features/Artist/Project.swift
+++ Modules/Features/Artist/Project.swift
@@ -6,7 +6,13 @@ let name = "Artist"
 let project = Project.featureModule(
   name: name,
   platform: .iOS.with(supportedPlatforms: .macOS),
+  exampleAppPlatform: .iOS,
   dependencies: [
     .foundation("Core"),
+    .foundation("UI"),
+    .foundation("Networking"),
+  ],
+  additionalTestDependencies: [
+    .project(target: "NetworkTesting", path: .relativeToRoot("Modules/Foundation/Networking/")),
   ]
 )
