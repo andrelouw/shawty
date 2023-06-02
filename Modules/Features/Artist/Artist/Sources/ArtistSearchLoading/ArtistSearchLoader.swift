dadabbc8cf07081f6ff17ec1536a869b9b79aeee
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-02 23:31:16 +0200

    Confirm AtistSearchLoader to QueryLoader

--- Modules/Features/Artist/Artist/Sources/ArtistSearchLoading/ArtistSearchLoader.swift
+++ Modules/Features/Artist/Artist/Sources/ArtistSearchLoading/ArtistSearchLoader.swift
@@ -1,3 +1,3 @@
-public protocol ArtistSearchLoader {
-  func load(with query: String) async throws -> [Artist]
-}
+import Shared
+
+public protocol ArtistSearchLoader: QueryValueLoader where Input == String, Output == [Artist] { }
