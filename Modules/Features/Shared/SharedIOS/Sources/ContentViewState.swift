56396d8526d00a98beb10b0ea7950ec1d7820de8
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-02 19:22:47 +0200

    Add some convenience types on ContentViewState for ease of use

--- Modules/Features/Shared/SharedIOS/Sources/ContentViewState.swift
+++ Modules/Features/Shared/SharedIOS/Sources/ContentViewState.swift
@@ -5,4 +5,24 @@ public enum ContentViewState<Content> {
   case screenNotice(ScreenNoticeModel)
   case loaded(Content)
   case error(String)
+
+  public static var idle: Self {
+    ContentViewState.screenNotice(.init(title: "", icon: .headphones))
+  }
+
+  public static var empty: Self {
+    ContentViewState.screenNotice(.init(title: "", icon: .noResults))
+  }
+
+  public static var noSearch: Self {
+    ContentViewState.screenNotice(.init(title: "", icon: .search))
+  }
+
+  public var isLoading: Bool {
+    if case .loading = self {
+      return true
+    }
+
+    return false
+  }
 }
