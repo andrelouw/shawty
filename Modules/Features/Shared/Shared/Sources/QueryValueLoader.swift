34109e5dc48e11afb50178533b2f83343397aa17
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-02 23:17:55 +0200

    Add QueryValueLoader as a generic signature loading some output async with a given input

--- Modules/Features/Shared/Shared/Sources/QueryValueLoader.swift
+++ Modules/Features/Shared/Shared/Sources/QueryValueLoader.swift
@@ -1,5 +1,5 @@
 public protocol QueryValueLoader {
   associatedtype Output
   associatedtype Input
-  func load(with input: Input) async throws -> Output
+  func load(_ input: Input) async throws -> Output
 }
