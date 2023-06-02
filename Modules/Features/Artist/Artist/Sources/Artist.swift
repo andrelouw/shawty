c4e902007c1e2074746660d5842b7f3967d37303
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-02 14:53:58 +0200

    Add Artist entity

--- Modules/Features/Artist/Artist/Sources/Artist.swift
+++ Modules/Features/Artist/Artist/Sources/Artist.swift
@@ -1,1 +1,13 @@
-public struct Artist { }
+import Foundation
+
+public struct Artist: Equatable {
+  public let id: Int
+  public let name: String
+  public let imageURL: URL
+
+  public init(id: Int, name: String, imageURL: URL) {
+    self.id = id
+    self.name = name
+    self.imageURL = imageURL
+  }
+}
