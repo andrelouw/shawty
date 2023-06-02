84bc9656439ba753473a309085c314dc347cd394
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-02 16:51:08 +0200

    Add a ScreenNoticeView with ScreenNoticeModel to aid in full screen notice messages, error, warnings ats

---
+++ Modules/Foundation/UI/UI/Sources/Extensions/Views/ScreenNotice/ScreenNoticeModel.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+public struct ScreenNoticeModel: Equatable {
+  public enum Style {
+    case prominent
+    case watermark
+
+    var opacity: Double {
+      if case .watermark = self {
+        return 0.5
+      }
+      return 1.0
+    }
+  }
+
+  public let title: String
+  public let subtitle: String?
+  public let icon: Icon
+  public let iconColor: Color
+  public let style: Style
+
+  public init(
+    title: String,
+    subtitle: String? = nil,
+    icon: Icon,
+    iconColor: Color = .gray,
+    style: Style = .prominent
+  ) {
+    self.title = title
+    self.subtitle = subtitle
+    self.icon = icon
+    self.iconColor = iconColor
+    self.style = style
+  }
+}
