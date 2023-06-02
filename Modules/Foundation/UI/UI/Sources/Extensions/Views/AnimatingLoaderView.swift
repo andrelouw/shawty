2c8737152405a2fa965f23723c0b675cd10c58e7
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-02 16:46:42 +0200

    Add a fancy animating loader view

---
+++ Modules/Foundation/UI/UI/Sources/Extensions/Views/AnimatingLoaderView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+// Reference: https://betterprogramming.pub/create-an-awesome-loading-state-using-swiftui-9815ff6abb80
+public struct AnimatingLoaderView: View {
+  private var color: Color = .brand.primary
+  private var diameter: CGFloat = 10
+
+  public init() { }
+
+  @State private var animating = false
+
+  public var body: some View {
+    HStack {
+      animatingCircle()
+      animatingCircle(withDelay: 0.3)
+      animatingCircle(withDelay: 0.6)
+    }
+    .onAppear {
+      withAnimation {
+        animating = true
+      }
+    }
+  }
+
+  private func animatingCircle(withDelay duration: Double = 0) -> some View {
+    Circle()
+      .fill(color)
+      .frame(width: diameter, height: diameter)
+      .scaleEffect(animating ? 1.0 : 0.5)
+      .animation(
+        .easeInOut(duration: 0.5)
+          .repeatForever()
+          .delay(duration),
+        value: animating
+      )
+  }
+}
+
+struct ListCellView_Previews: PreviewProvider {
+  static var previews: some View {
+    AnimatingLoaderView()
+  }
+}
