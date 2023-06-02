461bdb05b14e1c97ba0fa0d466e4a10c1436b6f3
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-02 20:29:32 +0200

    Build out basic ImageTitleRowView

---
+++ Modules/Features/Shared/SharedIOS/Sources/ImageTitleRow/ImageTitleRowView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+import UI
+
+struct ImageTitleRowView: View {
+  var title: String
+  var loadingImage: LoadingImage
+
+  var body: some View {
+    HStack {
+      imageView
+      Text(title)
+      Spacer()
+      Image(systemName: "chevron.forward")
+        .font(.footnote.bold())
+        .foregroundColor(Color(UIColor.tertiaryLabel))
+        .padding(.leading)
+    }
+  }
+
+  @ViewBuilder private var imageView: some View {
+    LoadingImageView(loadingImage: loadingImage)
+      .frame(width: 50, height: 50)
+      .clipped()
+      .clipShape(RoundedRectangle(cornerRadius: 5))
+  }
+}
+
+struct ImageTitleRowView_Previews: PreviewProvider {
+  static var previews: some View {
+    ImageTitleRowView(
+      title: "Title",
+      loadingImage: .loading
+    )
+    .background(Color.background.primary)
+  }
+}
