5a853acff93267d8ff3c1f3d370119cc135711e1
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-03 01:40:42 +0200

    Add the ArtistSearchSceneCoordinator which is the main coordinator for the app

---
+++ Apps/iOS/iOSApp/Sources/ArtistSearch/ArtistSearchSceneCoordinator.swift
@@ -0,0 +1,52 @@
+import ArtistIOS
+import Shared
+import UI
+import UIKit
+
+final class ArtistSearchSceneCoordinator: Coordinator {
+  public var childCoordinators = [Coordinator]()
+  public let navigationController: UINavigationController
+  private lazy var featureFactory: FeatureFactory = .live
+
+  public init(
+    with navigationController: UINavigationController
+  ) {
+    self.navigationController = navigationController
+  }
+
+  public func start() {
+    let coordinator = artistSearchCoordinator()
+    addChild(coordinator)
+    // TODO: WeakReference Proxy
+    coordinator.delegate = self
+
+    coordinator.start()
+  }
+
+  private func artistSearchCoordinator() -> ArtistSearchCoordinator {
+    ArtistSearchCoordinator(
+      navigationController: navigationController,
+      featureFactory: featureFactory,
+      removeCoordinatorWith: removeChild
+    )
+  }
+}
+
+extension ArtistSearchSceneCoordinator: ArtistSearchCoordinatorDelegate {
+  func didSelectArtist(withID id: Int) {
+    let alert = UIAlertController(
+      title: "Artist Selected",
+      message: "Artist ID: \(id)",
+      preferredStyle: .alert
+    )
+
+    alert.addAction(
+      UIAlertAction(
+        title: "OK",
+        style: .default
+      )
+    )
+
+    navigationController.present(alert, animated: true)
+  }
+}
