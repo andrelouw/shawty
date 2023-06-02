95c71fdc9c18116cde7d157e6ef6928dcea79094
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-02 13:54:25 +0200

    Rename project  accordingly

--- Tuist/ProjectDescriptionHelpers/ProjectConfiguration.swift
+++ Tuist/ProjectDescriptionHelpers/ProjectConfiguration.swift
@@ -2,8 +2,8 @@ import ProjectDescription
 
 public enum ProjectConfiguration {
   public static let organizationName = "andrelouw"
-  public static let bundleID = "com.andrelouw"
-  public static let appName = "MyApp"
+  public static let bundleID = "com.annabananas"
+  public static let appName = "Shawty"
   public static let iOSVersion = "16.4"
   public static let macOSVersion = "13.2"
 }
