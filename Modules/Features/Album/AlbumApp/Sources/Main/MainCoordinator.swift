6f52b8b3d6a255a713a093966a9a0bae72eaa730
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-03 02:46:21 +0200

    Fix wrong file templates

--- Modules/Features/Album/AlbumApp/Sources/Main/MainCoordinator.swift
+++ Modules/Features/Album/AlbumApp/Sources/Main/MainCoordinator.swift
@@ -1,23 +1,15 @@
 import Album
-import Core
+import UI
 import UIKit
 
 public final class MainCoordinator: Coordinator {
-  private var navigationController: UINavigationController
-  private var childCoordinators = [Coordinator]()
+  public var navigationController: UINavigationController
+  public var childCoordinators = [Coordinator]()
 
   public init(appWindow: UIWindow) {
     navigationController = UINavigationController()
     appWindow.rootViewController = navigationController
   }
 
-  public func start() {
-    let coordinator = AlbumCoordinator(
-      appName: Bundle.main.displayName ?? "App",
-      navigationController: navigationController
-    )
-
-    coordinator.start()
-    childCoordinators = [coordinator]
-  }
+  public func start() { }
 }
