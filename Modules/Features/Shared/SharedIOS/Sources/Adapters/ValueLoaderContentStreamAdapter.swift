8f1f4dfc9a64ba86810d1c1f2ddbc76c6ff0b520
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-03 03:12:51 +0200

    Add ValueLoaderContentStreamAdapter to adapt any view model or models to a content view stream

---
+++ Modules/Features/Shared/SharedIOS/Sources/Adapters/ValueLoaderContentStreamAdapter.swift
@@ -0,0 +1,51 @@
+import Foundation
+import Shared
+
+public struct ValueLoaderContentStreamAdapter<Loader: ValueLoader> {
+  public typealias EmptyEvaluation = (Loader.Output) -> Bool
+  private let loader: Loader
+  private let isEmptyEvaluation: EmptyEvaluation
+
+  public init(
+    loader: Loader,
+    isEmptyEvaluation: @escaping EmptyEvaluation
+  ) {
+    self.loader = loader
+    self.isEmptyEvaluation = isEmptyEvaluation
+  }
+
+  public func load() -> ContentViewStream<Loader.Output> {
+    AsyncStream { continuation in
+      Task {
+        do {
+          continuation.yield(.loading)
+          let value = try await loader.load()
+
+          guard !isEmpty(value) else {
+            continuation.yield(.empty)
+            continuation.finish()
+            return
+          }
+
+          continuation.yield(.loaded(value))
+        } catch {
+          continuation.yield(.error(SharedIOSStrings.fullScreenConnectionError))
+        }
+        continuation.finish()
+      }
+    }
+  }
+
+  private func isEmpty(_ value: Loader.Output) -> Bool {
+    isEmptyEvaluation(value)
+  }
+}
+
+extension ValueLoaderContentStreamAdapter where Loader.Output: Collection {
+  public init(loader: Loader) {
+    self.init(
+      loader: loader,
+      isEmptyEvaluation: { $0.isEmpty }
+    )
+  }
+}
