9042107d5bb83092258863a45e522270c087a1db
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-03 05:28:22 +0200

    Connect the TrackListCoordinator to Examples apps main coordinator

--- Modules/Features/Track/TrackApp/Sources/Main/MainCoordinator.swift
+++ Modules/Features/Track/TrackApp/Sources/Main/MainCoordinator.swift
@@ -1,15 +1,59 @@
+import Core
+import Networking
+import Shared
+import SharedIOS
 import Track
+import TrackIOS
 import UI
 import UIKit
 
-public final class MainCoordinator: Coordinator {
-  public var navigationController: UINavigationController
-  public var childCoordinators = [Coordinator]()
+final class MainCoordinator: Coordinator, FeatureFactory {
+  var navigationController: UINavigationController
+  var childCoordinators = [Coordinator]()
 
-  public init(appWindow: UIWindow) {
+  let baseURL = URL(string: "https://api.deezer.com")!
+  lazy var httpClient: HTTPClient = URLSessionHTTPClient(session: .shared)
+  lazy var imageLoader: any ImageDataLoader = RemoteImageDataLoader(client: httpClient)
+
+  init(appWindow: UIWindow) {
     navigationController = UINavigationController()
     appWindow.rootViewController = navigationController
   }
 
-  public func start() { }
+  func start() {
+    let coordinator = trackListCoordinator()
+    addChild(coordinator)
+    // TODO: WeakReference Proxy
+    coordinator.delegate = self
+
+    coordinator.start()
+  }
+
+  private func trackListCoordinator() -> TrackListCoordinator {
+    TrackListCoordinator(
+      trackListURL: baseURL.appending(path: "album/373401057/tracks"),
+      navigationController: navigationController,
+      featureFactory: self,
+      removeCoordinatorWith: removeChild
+    )
+  }
+}
+
+extension MainCoordinator: TrackListCoordinatorDelegate {
+  func didSelectTrack(withID id: Int) {
+    let alert = UIAlertController(
+      title: "Track Selected",
+      message: "Track ID: \(id)",
+      preferredStyle: .alert
+    )
+
+    alert.addAction(
+      UIAlertAction(
+        title: "OK",
+        style: .default
+      )
+    )
+
+    navigationController.present(alert, animated: true)
+  }
 }
