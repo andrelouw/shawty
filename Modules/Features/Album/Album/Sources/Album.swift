a2b4578ee9c5b9a4520743a5f1c37c4079ff17ce
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-03 02:32:24 +0200

    Add album entity

--- Modules/Features/Album/Album/Sources/Album.swift
+++ Modules/Features/Album/Album/Sources/Album.swift
@@ -1,1 +1,23 @@
-public struct Album { }
+import Foundation
+
+public struct Album: Equatable {
+  public let id: Int
+  public let title: String
+  public let imageURL: URL
+  public let releaseDate: Date
+  public let hasExplicitLyrics: Bool
+
+  public init(
+    id: Int,
+    title: String,
+    imageURL: URL,
+    releaseDate: Date,
+    hasExplicitLyrics: Bool
+  ) {
+    self.id = id
+    self.title = title
+    self.imageURL = imageURL
+    self.releaseDate = releaseDate
+    self.hasExplicitLyrics = hasExplicitLyrics
+  }
+}
