9c56b569ade7967c8d893dc2b3d46cee84449e7b
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-04 16:47:53 +0200

    Fix small typo which was preventing the linting script from succeeding

--- Scripts/lint.sh
+++ Scripts/lint.sh
@@ -6,7 +6,7 @@ if [[ "$(uname -m)" == arm64 ]]; then
     export PATH="/opt/homebrew/bin:$PATH"
 fi
 
-if which mint which swiftlints> /dev/null; then
+if which mint which swiftlint> /dev/null; then
   mint run swiftlint lint  --config $DIR/../.swiftlint.yml
 else
   echo "warning: Swiftlint not installed with Mint, Linting won't work. Install using 'make bootstrap'. "
