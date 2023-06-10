063e7d2776d91dc9a6c952b4a3472bba9cc6daef
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-10 13:09:35 +0200

    Add waitUntilExists helper method on XCUIElement

--- Apps/iOS/iOSAppUIAcceptanceTests/Tests/AppUIAcceptanceTests.swift
+++ Apps/iOS/iOSAppUIAcceptanceTests/Tests/AppUIAcceptanceTests.swift
@@ -1,11 +1,13 @@
 import SharedIOS
+import Testing
 import XCTest
 
 final class AppUIAcceptanceTests: XCTestCase {
   func test_appLaunch() {
     let app = XCUIApplication()
     app.launch()
+    let noResultText = app.staticTexts[SharedIOSStrings.noSearchTitle].waitUntilExists()
 
-    XCTAssertTrue(app.staticTexts[SharedIOSStrings.noSearchTitle].exists)
+    XCTAssertTrue(noResultText.exists)
   }
 }
---
+++ Modules/Foundation/Testing/Testing/Sources/Extensions/XCUIElement.swift
@@ -0,0 +1,21 @@
+//
+
+import XCTest
+
+extension XCUIElement {
+  @discardableResult
+  public func waitUntilExists(
+    timeout: TimeInterval = 5,
+    file: StaticString = #filePath,
+    line: UInt = #line
+  ) -> XCUIElement {
+    let elementExists = waitForExistence(timeout: timeout)
+    if elementExists {
+      return self
+    } else {
+      XCTFail("Could not find \(self)", file: file, line: line)
+    }
+
+    return self
+  }
+}
