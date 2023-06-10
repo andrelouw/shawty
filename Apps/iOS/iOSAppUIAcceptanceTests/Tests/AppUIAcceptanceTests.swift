04c638726588e5f005e8490c48c35109e55efc9d
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-10 13:22:53 +0200

    Create ARtistSearchScreen DSL for no results title

--- Apps/iOS/iOSAppUIAcceptanceTests/Tests/AppUIAcceptanceTests.swift
+++ Apps/iOS/iOSAppUIAcceptanceTests/Tests/AppUIAcceptanceTests.swift
@@ -2,10 +2,8 @@ import SharedIOS
 import Testing
 import XCTest
 
-final class AppUIAcceptanceTests: UITestCase {
-  func test_appLaunch() {
-    let noResultText = app.staticTexts[SharedIOSStrings.noSearchTitle].waitUntilExists()
-
-    XCTAssertTrue(noResultText.exists)
+final class AppUIAcceptanceTests: UITestCase, ArtistSearchScreen {
+  func test_onLaunch_artistSearchShowsNoSearchActiveNotice() {
+    XCTAssertTrue(noResultsTitle().exists)
   }
 }
---
+++ Apps/iOS/iOSAppUIAcceptanceTests/Tests/ArtistSearchScreen.swift
@@ -0,0 +1,24 @@
+import ArtistIOS
+import SharedIOS
+import Testing
+import XCTest
+
+protocol ArtistSearchScreen { }
+
+extension ArtistSearchScreen where Self: UITestCase {
+  //  func artistSearchNavigationBar(
+  //    file: StaticString = #filePath,
+  //    line: UInt = #line
+  //  ) -> XCUIElement {
+  //    app.navigationBars[ArtistIOSStrings.artistSearchScreenTitle]
+  //      .waitUntilExists(file: file, line: line)
+  //  }
+
+  func noResultsTitle(
+    file: StaticString = #filePath,
+    line: UInt = #line
+  ) -> XCUIElement {
+    app.staticTexts[SharedIOSStrings.noSearchTitle]
+      .waitUntilExists(file: file, line: line)
+  }
+}
