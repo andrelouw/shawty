65ba4bf8b8b799c8f03d43ba9fc8d7ea2cfda93a
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-10 13:05:05 +0200

    App shows no search title when loaded

--- Apps/iOS/iOSAppUIAcceptanceTests/Tests/AppUIAcceptanceTests.swift
+++ Apps/iOS/iOSAppUIAcceptanceTests/Tests/AppUIAcceptanceTests.swift
@@ -1,8 +1,11 @@
+import SharedIOS
 import XCTest
 
 final class AppUIAcceptanceTests: XCTestCase {
   func test_appLaunch() {
     let app = XCUIApplication()
     app.launch()
+
+    XCTAssertTrue(app.staticTexts[SharedIOSStrings.noSearchTitle].exists)
   }
 }
