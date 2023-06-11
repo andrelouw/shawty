02f8bf415fd3140101ff38fe5897225f417b7eae
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-11 17:14:40 +0200

    Hookup AlbumListUIComposer in ios app Feature Factory

--- Apps/iOS/iOSApp/Sources/FeatureFactory/LiveFeatureFactory+Album.swift
+++ Apps/iOS/iOSApp/Sources/FeatureFactory/LiveFeatureFactory+Album.swift
@@ -1,12 +1,18 @@
+import Album
 import AlbumIOS
+import Artist
 import UIKit
 
 extension LiveFeatureFactory: AlbumFactory {
   func makeAlbumListViewController(
-    for _: Int,
-    onAlbumSelection _: @escaping (Int) -> Void
+    for albumID: Int,
+    onAlbumSelection: @escaping (Int) -> Void
   ) -> UIViewController {
-    .init()
+    AlbumListUIComposer.listComposedWith(
+      albumsLoader: makeRemoteAlbumsLoader(for: albumID),
+      imageDataLoader: imageLoader,
+      selection: onAlbumSelection
+    )
   }
 
   func makeAlbumDetailViewController(
@@ -14,5 +20,12 @@ extension LiveFeatureFactory: AlbumFactory {
     onTrackSelection _: @escaping (Int) -> Void
   ) -> UIViewController {
     .init()
+  }
+
+  private func makeRemoteAlbumsLoader(for albumID: Int) -> any AlbumsLoader {
+    RemoteAlbumsLoader(
+      url: ArtistEndpoint.albums(id: albumID).url(baseURL: baseURL),
+      client: httpClient
+    )
   }
 }
