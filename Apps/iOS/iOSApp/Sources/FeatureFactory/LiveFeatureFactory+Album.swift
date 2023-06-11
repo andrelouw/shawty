5016dcafd9612d3a1180467518603ebdf7c211c8
annasbananas <135325153+annasbananas@users.noreply.github.com>
2023-06-11 17:44:54 +0200

    Hookup album detail view into ios app

--- Apps/iOS/iOSApp/Sources/FeatureFactory/LiveFeatureFactory+Album.swift
+++ Apps/iOS/iOSApp/Sources/FeatureFactory/LiveFeatureFactory+Album.swift
@@ -1,6 +1,7 @@
 import Album
 import AlbumIOS
 import Artist
+import SwiftUI
 import UIKit
 
 extension LiveFeatureFactory: AlbumFactory {
@@ -16,15 +17,26 @@ extension LiveFeatureFactory: AlbumFactory {
   }
 
   func makeAlbumDetailViewController(
-    for _: Int,
+    for albumID: Int,
     onTrackSelection _: @escaping (Int) -> Void
   ) -> UIViewController {
-    .init()
+    AlbumDetailUIComposer.detailComposedWith(
+      albumLoader: makeRemoteAlbumLoader(for: albumID),
+      imageDataLoader: imageLoader,
+      listView: { EmptyView() }
+    )
   }
 
   private func makeRemoteAlbumsLoader(for albumID: Int) -> any AlbumsLoader {
     RemoteAlbumsLoader(
       url: ArtistEndpoint.albums(id: albumID).url(baseURL: baseURL),
+      client: httpClient
+    )
+  }
+
+  private func makeRemoteAlbumLoader(for albumID: Int) -> any AlbumLoader {
+    RemoteAlbumLoader(
+      url: AlbumEndpoint.album(id: albumID).url(baseURL: baseURL),
       client: httpClient
     )
   }
